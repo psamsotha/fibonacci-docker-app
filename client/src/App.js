9f9d7fc81a2f1501ce7aae05e285b0bd97919cc7
Paul Samsotha <paulsamsotha.dev@gmail.com>
2021-06-07 15:30:02 -0700

    Add title to React app

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h2>Fib Calculator</h2>
           <div>
             <Link className="App-link App-link-home" to="/">Home</Link>
             <Link className="App-link App-link-home" to="/otherpage">Other Page</Link>
